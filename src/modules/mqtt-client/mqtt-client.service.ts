7f931ec71e2e9e193feb5c98de22e6ce0a347147
Jonas Florian Reinbold <uk063832@student.uni-kassel.de>
2022-02-22 18:01:47 +0100

    new mqtt publish

--- src/modules/mqtt-client/mqtt-client.service.ts
+++ src/modules/mqtt-client/mqtt-client.service.ts
@@ -146,7 +146,7 @@ export class MqttClientService implements OnModuleInit {
   }
 
   async publish(topic: string, payload: any) {
-    const response = this.client.publish(topic, payload);
+    const response = this.client.publish(topic, payload.stringify());
     console.log(
       '\n' +
         topic +
